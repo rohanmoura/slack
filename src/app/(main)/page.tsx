143d98090ed29d0496fdd65392427906212a1574
rohanmoura <rsmoura29@gmail.com>
2024-07-06 16:05:11 +0530

    get current user session

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,29 @@
+import { getUserData } from "@/actions/get-user-data";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+
+  const userData = await getUserData()
+
+  if (!userData) {
+    return redirect("/auth")
+  }
+
+  const userWorkspaceId = userData.workplaces?.[0]
+
+  if(!userWorkspaceId) {
+    return redirect("/create-workspace")
+  }
+
+  if(userWorkspaceId){
+    return redirect(`/workspace/${userWorkspaceId}`)
+  }
+
+  return (
+    <div className="flex h-full w-full justify-center items-center">
+      <h1>Home</h1>
+      <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+    </div>
+  );
+}
